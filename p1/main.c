2586391515c1f89698e582a76faebf60bf67ecf1
2025050204 <choiseoyoung@kau.kr>
2026-04-29 16:09:48 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -5,17 +5,23 @@ int main(void){
     
     scanf("%d", &N);
     
-    for(int i=1; ; i++){
-        for(int j =2; ; j*=2){
-            for(int k =1; ; k++){
-                if(900*i + 750*j + 200*k > N || k < i || k < j){
-                    printf("none");
+    for(int i=1; 900*i<=N; i++){
+        for(int j =1; 900*i + 750*j<=N; j+=2){
+            for(int k =1; 900*i + 750*j + 200*k <= N; k++){
+                int gkq = 900*i + 750*j + 200*k;
+                if(gkq <= N){
+                    if(k<i || k <j){
+                        printf("%d %d %d\n", i, j, k);
+                    }
                     break;
-                    
+                }
+                if(gkq>N){
+                    break;
                 }else{
-                    printf("%d %d %d\n", i, j ,k);
+                    printf("none");
                 }
             }
         }
     }
+    return 0;
 }
