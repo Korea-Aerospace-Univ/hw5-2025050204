15bf93694d2f46ac2131412432029c09f1d0bb37
2025050204 <choiseoyoung@kau.kr>
2026-04-29 16:46:42 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -6,18 +6,14 @@ int main(void){
     scanf("%d", &N);
     
     for(int i=1; 900*i<=N; i++){
-        for(int j =2; 900*i + 750*j<=N; j+=2){
-            for(int k =1; 900*i + 750*j + 200*k <= N; k++){
+        for(int j =2; 750*j<=N; j+=2){
+            for(int k =1; 200*k <= N; k++){
                 int gkq = 900*i + 750*j + 200*k;
                 if(gkq == N){
                     if(k<i || k <j){
                         printf("%d %d %d\n", i, j, k);
                         rs=1;
                     }
-                    break;
-                if(gkq>N){
-                    break;
-                }
                 
                 }
             }   
